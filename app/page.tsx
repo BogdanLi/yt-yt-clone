88ea227860253b64b8d9a6da78a3c6f8b6b20a50
BogdanLi <li.bogdan44@gmail.com>
2024-08-26 15:04:33 +0500

    Added tech scetion

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import HeroSection from "@/components/Sections/SectionHero";
+import TechSection from "@/components/Sections/SectionTech";
 
 const Home = () => {
   return (
     <main>
       <HeroSection />
+      <TechSection />
     </main>
   );
 };
